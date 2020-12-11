b8a8bb34af9a9477f1fb6e571e92dd94acf7eda1
Professor Isidro <fmassetto@gmail.com>
2020-12-11 19:36:32 -0300

    changing deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PID=$(ps x | grep vitrinevirtual | tail -1 | cut -d" " -f1)
+PID=$(ps x | grep gamecenter | tail -1 | cut -d" " -f1)
 kill -9 $PID
-cp ./target/testador-0.0.1-SNAPSHOT.jar /root
-nohup java -jar /root/testador-0.0.1-SNAPSHOT.jar & 
+cp ./target/gamecenter-0.0.1-SNAPSHOT.jar /root
+nohup java -jar /root/gamecenter-0.0.1-SNAPSHOT.jar & 
